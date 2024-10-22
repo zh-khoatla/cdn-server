b9620e63c19da91500cfe2f7156a611dc5e70c27
anhkhoatranle30 <anhkhoatranle30@gmail.com>
2024-10-22 10:31:33 +0700

    feat: Update with client new way of downloading CDN

--- src/api/v1/components/bundle/controller/getBundle.js
+++ src/api/v1/components/bundle/controller/getBundle.js
@@ -3,7 +3,17 @@ import path from 'path';
 const getBundle = async (req, res) => {
   try {
     const fileName = req.params.bundleName;
-    const filePath = path.join('src', 'api', 'v1', 'bundles', fileName);
+    const version = req.params.version;
+
+    const filePath = path.join(
+      'src',
+      'api',
+      'v1',
+      'bundles',
+      version,
+      fileName
+    );
+
     console.log('🚀 ~ file: getBundle.js:7 ~ getBundle ~ filePath:', filePath);
 
     res.download(filePath, fileName, (err) => {
